1f29cb24834e105a10b84485abc6a49fc3e26fc8
yyklimenko <yyklimenko@gmail.com>
2023-01-02 17:13:09 +0600

    removed: tasks have removed

--- market/tasks/config.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-celery = Celery('mailing')
-celery.conf.broker_url = 'redis://localhost:6379/0'
-celery.conf.result_backend = 'redis://localhost:6379/0'
-
-
--- market/tasks/mailing.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-from market.tasks.config import celery
-
-
-@celery.task
-def hello():
-    # sleep(100000)
-    return '1'
